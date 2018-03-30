497a3b41e8867532320331581fc6a3a6ac910562
claudineiarneckedelima <claudinei-de-lima@hotmail.com>
2018-03-30 14:06:11 -0300

    Update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->char('role', 1)->default('C');
             $table->rememberToken();
             $table->timestamps();
         });
